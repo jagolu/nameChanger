2382d03872da02f15bfb20f18813ca8a192630c0
Jagolu <javo14795@gmail.com>
2018-09-12 23:00:56 +0200

    some functions added and fixed and some private vars
    addVar ---------eraseAccentMarks
    addVar ---------deleteLastSpace
    fix --------------areParamsRight
    addFunction---changeTheNames
    fixFunction-----listFilesOfADir
    addFunction---prueba(DELETE)
    addFunction---beginWithMayus
    addFunction---changeWithLowBar
    addFunction---changeWithMayus
    addFunction---changeWithoutMayus
    addFunction---eraseAccentMarks

--- src/namechanger/NameChanger.java
+++ src/namechanger/NameChanger.java
@@ -13,12 +13,15 @@ public class NameChanger {
     private boolean changeWithoutMayus = false;
     private boolean changeDirsName = false;
     private boolean changeFileName = false;
+    private boolean eraseAccentMarks = false;
+    private boolean deleteLastSpace = false;
     private File rootDir = null;
     
     public NameChanger(){}
     
     public NameChanger(boolean changeSubDir, boolean beginWithMayus,boolean changeWithLowBar, boolean changeWithScript,
-            boolean changeWithMayus, boolean changeWithoutMayus, boolean changeDirsName, boolean changeFileName, String rootDir){
+            boolean changeWithMayus, boolean changeWithoutMayus, boolean changeDirsName, boolean changeFileName, 
+            boolean eraseAccentMarks, boolean deleteLastSpace, String rootDir){
         this.changeSubDir = changeDirsName;
         this.beginWithMayus = beginWithMayus;
         this.changeWithLowBar = changeWithLowBar;
@@ -27,14 +30,19 @@ public class NameChanger {
         this.changeWithoutMayus = changeWithoutMayus;
         this.changeDirsName = changeDirsName;
         this.changeFileName = changeFileName;
+        this.eraseAccentMarks = eraseAccentMarks;
+        this.deleteLastSpace = deleteLastSpace;
         this.rootDir = new File(rootDir);
     }
     
     public boolean areTheParamsRight(){
-        if(!this.changeWithLowBar && !this.changeWithScript && !this.changeWithMayus && !this.changeWithoutMayus && !this.beginWithMayus) return false;
+        if(!this.changeWithLowBar && !this.changeWithScript && !this.changeWithMayus && 
+                !this.changeWithoutMayus && !this.beginWithMayus && !this.eraseAccentMarks &&
+                !this.deleteLastSpace) return false;
         else if(!this.changeDirsName && !this.changeFileName) return false;
         else if(this.changeWithLowBar && this.changeWithScript) return false;
-        else if((this.changeWithLowBar || this.changeWithScript) && (this.changeWithMayus || this.changeWithoutMayus)) return false;
+        else if((this.changeWithLowBar || this.changeWithScript) && 
+                (this.changeWithMayus || this.changeWithoutMayus)) return false;
         else if(!this.changeDirsName && !this.changeFileName) return false;
         else if(!existsTheDir(this.rootDir)) return false;
         else return true;
@@ -45,17 +53,162 @@ public class NameChanger {
         else return false;
     }
     
+    public void changeTheNames(){
+        ArrayList<File> files = this.listFilesOfADir(this.rootDir);
+        for(int i=0;i<files.size();i++){
+            System.out.println(files.get(i).getAbsolutePath());
+        }
+    }
+    
     private ArrayList<File> listFilesOfADir(File dir){
-        ArrayList<File> files = new ArrayList<File> ();
+        ArrayList<File> files = new ArrayList<> ();
         for (final File ficheroEntrada : dir.listFiles()) {
             if (ficheroEntrada.isDirectory()) {
                 files.add(ficheroEntrada);
-                listFilesOfADir(ficheroEntrada);
+                files.addAll(listFilesOfADir(ficheroEntrada));
             } 
             else {
                 files.add(ficheroEntrada);
             }
         }
         return files;
+    }
+    
+    public void prueba(String prueba){
+        System.out.println(this.beginWithMayus(prueba));
+        System.out.println(this.changeWithLowBar(prueba));
+        System.out.println(this.changeWithScript(prueba));
+        System.out.println(this.changeWithMayus(prueba));
+        System.out.println(this.changeWithoutMayus(prueba));
+        System.out.println(this.eraseAccentMarks(prueba));
+    }
+    
+    private String beginWithMayus(String name){
+        String first = Character.toString(Character.toUpperCase(name.charAt(0)));
+        return first + name.substring(1, name.length()-1);
+    }
+    
+    private String changeWithLowBar(String name){
+        return name.replace(' ', '_');
+    }
+    
+    private String changeWithScript(String name){
+        return name.replace(' ', '-');
+    }
+    
+    private String changeWithMayus(String name){
+        StringBuffer SBName = new StringBuffer();
+        boolean lastSpace = false;
+        for(int i=0;i<name.length();i++){
+            if(!lastSpace && name.charAt(i)!=' '){
+                SBName.append(name.charAt(i));
+            }
+            else if(!lastSpace && name.charAt(i)==' '){
+                lastSpace = true;
+            }
+            else if(lastSpace){
+                SBName.append(Character.toUpperCase(name.charAt(i)));
+                lastSpace = false;
+            }
+        }       
+       return SBName.toString();
+    }
+    
+    private String changeWithoutMayus(String name){
+        StringBuffer SBName = new StringBuffer();
+        for(int i=0;i<name.length();i++){
+            if(name.charAt(i)!=' ') SBName.append(name.charAt(i));
+        }       
+       return SBName.toString();
+    }
+    
+    private String eraseAccentMarks(String name){
+        StringBuffer SBName = new StringBuffer();
+        for(int i=0;i<name.length();i++){
+            switch(name.charAt(i)){
+                case 'Á': SBName.append('A');
+                          break;
+                case 'À': SBName.append('A');
+                          break;
+                case 'Ä': SBName.append('A');
+                          break;
+                case 'Â': SBName.append('A');
+                          break;
+                case 'á': SBName.append('a');
+                          break;
+                case 'à': SBName.append('a');
+                          break;
+                case 'ä': SBName.append('a');
+                          break;
+                case 'â': SBName.append('a');
+                          break;
+                case 'É': SBName.append('E');
+                          break;
+                case 'È': SBName.append('E');
+                          break;
+                case 'Ë': SBName.append('E');
+                          break;
+                case 'Ê': SBName.append('E');
+                          break;
+                case 'é': SBName.append('e');
+                          break;
+                case 'è': SBName.append('e');
+                          break;
+                case 'ë': SBName.append('e');
+                          break;
+                case 'ê': SBName.append('e');
+                          break;
+                case 'Í': SBName.append('I');
+                          break;
+                case 'Ì': SBName.append('I');
+                          break;
+                case 'Ï': SBName.append('I');
+                          break;
+                case 'Î': SBName.append('I');
+                          break;
+                case 'í': SBName.append('i');
+                          break;
+                case 'ì': SBName.append('i');
+                          break;
+                case 'ï': SBName.append('i');
+                          break;
+                case 'î': SBName.append('i');
+                          break;
+                case 'Ó': SBName.append('O');
+                          break;
+                case 'Ò': SBName.append('O');
+                          break;
+                case 'Ö': SBName.append('O');
+                          break;
+                case 'Ô': SBName.append('O');
+                          break;
+                case 'ó': SBName.append('o');
+                          break;
+                case 'ò': SBName.append('o');
+                          break;
+                case 'ö': SBName.append('o');
+                          break;
+                case 'ô': SBName.append('o');
+                          break;
+                case 'Ú': SBName.append('U');
+                          break;
+                case 'Ù': SBName.append('U');
+                          break;
+                case 'Ü': SBName.append('U');
+                          break;
+                case 'Û': SBName.append('U');
+                          break;
+                case 'ú': SBName.append('u');
+                          break;
+                case 'ù': SBName.append('u');
+                          break;
+                case 'ü': SBName.append('u');
+                          break;
+                case 'û': SBName.append('u');
+                          break;
+                default : SBName.append(name.charAt(i));
+            }
+        }       
+       return SBName.toString();
     }
 }
---
+++ src/namechanger/main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package namechanger;
+
+/**
+ *
+ * @author Javi
+ */
+public class main {
+    public static void main(String [] args){
+        NameChanger nc = new NameChanger(true, true, true, true, true, true, true, true, true, true, "C:\\Users\\Javi\\Documents\\nameChanger");
+        //nc.changeTheNames();
+        nc.prueba("Ásdfasfi andfa aidfa aàëêÍÌÖÛ");
+    }
+}
